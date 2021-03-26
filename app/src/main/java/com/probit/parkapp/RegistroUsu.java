e47182b20379f10f1be5e63adb24d6b2d2ae2881
bonillasanti <61526552+bonillasanti@users.noreply.github.com>
2021-03-25 23:57:27 -0300

    parte de registro y drawable logo

---
+++ app/src/main/java/com/probit/parkapp/RegistroUsu.java
@@ -0,0 +1,12 @@
+package com.probit.parkapp;
+
+import android.widget.EditText;
+
+public class RegistroUsu  {
+
+    private EditText editTextName;
+    private EditText editTextEmail;
+    private EditText editTextPassword;
+
+
+}
