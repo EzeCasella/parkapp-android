a13aaa9afba59e6d890dfa67e7676ba368d5644f
Ezequiel Casella <ezequiel.casella@decemberlabs.com>
2021-04-07 22:23:02 -0300

    Cambiando logica de get parkings para MapViewModel

--- app/src/main/java/com/probit/parkapp/ui/map/MapsFragment.java
+++ app/src/main/java/com/probit/parkapp/ui/map/MapsFragment.java
@@ -3,22 +3,17 @@ package com.probit.parkapp.ui.map;
 import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.pm.PackageManager;
-import android.location.Location;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 import android.widget.Toast;
 
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -31,31 +26,51 @@ import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.probit.parkapp.R;
 import com.probit.parkapp.model.Parking;
-import com.probit.parkapp.repositories.ParkingsRepository;
-import com.probit.parkapp.ui.dashboard.DashboardViewModel;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class MapsFragment extends Fragment implements GoogleMap.OnMarkerClickListener, GoogleMap.OnMyLocationButtonClickListener, GoogleMap.OnMyLocationClickListener {
+public class MapsFragment extends Fragment {
 
 
     private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
     SupportMapFragment mapFragment;
     private MapsViewModel mViewModel;
-    boolean tienePermiso = false;
     private GoogleMap googleMap;
 //    UiSettings uiSettings = googleMap.getUiSettings();
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+
+        mViewModel = new ViewModelProvider(this).get(MapsViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_maps, container, false);
+
+        mViewModel.getParkingsLiveData().observe(getViewLifecycleOwner(), this::addMarkersAndMove);
+
+        return root;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(callback);
+
+        }
+    }
 
 
     private OnMapReadyCallback callback = new OnMapReadyCallback() {
 
-
-        private final int LOCATION_PERMISSION_REQUEST_CODE = 1;
-
-
         @Override
-        public void onMapReady(GoogleMap googleMap) {
+        public void onMapReady(GoogleMap _googleMap) {
+            googleMap = _googleMap;
 
             googleMap.setOnMarkerClickListener(onMarkerClickListener);
             googleMap.setOnInfoWindowClickListener(onInfoWindowClickListener);
@@ -64,48 +79,55 @@ public class MapsFragment extends Fragment implements GoogleMap.OnMarkerClickLis
             uiSettings.setMyLocationButtonEnabled(true);
             uiSettings.setMapToolbarEnabled(false);
 
+            mViewModel.onMapReady();
 
-            ParkingsRepository.getParkings(data -> {
-                ArrayList<Parking> parkings = (ArrayList<Parking>) data;
-                int i = 0;
-                float zoom = 13;
-                LatLng montevideo = new LatLng(-34.90328, -56.18816);
-                for (Parking park : parkings) {
-                    float latitud = Float.parseFloat(park.getLatitude());
-                    float longitud = Float.parseFloat(park.getLongitude());
-                    LatLng parking = new LatLng(latitud, longitud);
-                    googleMap.addMarker(new MarkerOptions().position(parking).title(park.getName())).setTag(i);
-                    i = i + 1;
-
-                }
-
-                googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(montevideo, zoom));
-            });
-
-
-            if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-
-                String permiso1 = Manifest.permission.ACCESS_FINE_LOCATION;
-                String permiso2 = Manifest.permission.ACCESS_COARSE_LOCATION;
-                List<String> permissionlist = new ArrayList<>();
-                permissionlist.add(permiso1);
-                permissionlist.add(permiso2);
-
-                ActivityCompat.requestPermissions(getActivity(), permissionlist.toArray(new String[permissionlist.size()]), LOCATION_PERMISSION_REQUEST_CODE);
-
-            } else {
-                googleMap.setMyLocationEnabled(true);
-
-            }
+            enableLocation();
         }
     };
 
+    private void addMarkersAndMove(ArrayList<Parking> parkings){
+        float zoom = 13;
+        LatLng montevideo = new LatLng(-34.90328, -56.18816);
+        for (Parking park : parkings) {
+            float latitud = Float.parseFloat(park.getLatitude());
+            float longitud = Float.parseFloat(park.getLongitude());
+            LatLng parking = new LatLng(latitud, longitud);
+            ;
+            googleMap.addMarker(new MarkerOptions().position(parking).title(park.getName())).setTag(parkings.indexOf(park));
+        }
+
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(montevideo, zoom));
+    }
+
+    private void enableLocation() {
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            String permiso1 = Manifest.permission.ACCESS_FINE_LOCATION;
+            String permiso2 = Manifest.permission.ACCESS_COARSE_LOCATION;
+            List<String> permissionlist = new ArrayList<>();
+            permissionlist.add(permiso1);
+            permissionlist.add(permiso2);
+
+            ActivityCompat.requestPermissions(getActivity(), permissionlist.toArray(new String[permissionlist.size()]), LOCATION_PERMISSION_REQUEST_CODE);
+
+        } else {
+            if (googleMap != null) {
+                googleMap.setMyLocationEnabled(true);
+            }
+
+        }
+    }
 
     GoogleMap.OnMarkerClickListener onMarkerClickListener = new GoogleMap.OnMarkerClickListener() {
         @Override
         public boolean onMarkerClick(Marker marker) {
             int position = (int) (marker.getTag());
-            Toast.makeText(getContext(), "Mando mensaje click" + position, Toast.LENGTH_SHORT).show();
+            Parking pk = mViewModel.getParkings().get(position);
+            Toast.makeText(getContext(), pk.getAddress(), Toast.LENGTH_SHORT).show();
+
+            // BottomSheetModalFragment
+
             return false;
         }
 
@@ -125,7 +147,8 @@ public class MapsFragment extends Fragment implements GoogleMap.OnMarkerClickLis
             case LOCATION_PERMISSION_REQUEST_CODE:
                 if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     // Permission Granted
-                    googleMap.setMyLocationEnabled(true);
+//                    googleMap.setMyLocationEnabled(true);
+                    enableLocation();
                 } else {
                     // Permission Denied
                     Toast.makeText(getActivity(), "No se aceptó permisos", Toast.LENGTH_LONG).show();
@@ -134,43 +157,6 @@ public class MapsFragment extends Fragment implements GoogleMap.OnMarkerClickLis
                 super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         }
     }
-
-
-    @Nullable
-    @Override
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             @Nullable ViewGroup container,
-                             @Nullable Bundle savedInstanceState) {
-
-        return inflater.inflate(R.layout.fragment_maps, container, false);
-
-       /* mViewModel =
-                new ViewModelProvider(this).get(MapsViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_maps, container, false);
-
-        return root;*/
-
-    }
-
-    /*@Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        mViewModel = new ViewModelProvider(this).get(MapsViewModel.class);
-        // TODO: Use the ViewModel
-    }*/
-
-    @Override
-    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
-        mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
-
-        if (mapFragment != null) {
-            mapFragment.getMapAsync(callback);
-
-        }
-    }
-
 
 
     @Override
@@ -199,21 +185,5 @@ public class MapsFragment extends Fragment implements GoogleMap.OnMarkerClickLis
     public void onStop() {
         super.onStop();
         mapFragment.onStop();
-    }
-
-    @Override
-    public boolean onMarkerClick(Marker marker) {
-        return false;
-    }
-
-    @Override
-    public boolean onMyLocationButtonClick() {
-        return false;
-
-    }
-
-    @Override
-    public void onMyLocationClick(@NonNull Location location) {
-
     }
 }
